ea92f8151e23bd148f9c0186fc82058685c793b2
Corbin Robb <corbinrobb@gmail.com>
2020-06-04 10:56:32 -0600

    Change req.user to have correct data

--- users/userRouter.js
+++ users/userRouter.js
@@ -15,7 +15,7 @@ router.post('/', validateUser, async (req, res) => {
 
 router.post('/:id/posts', validateUserId, validatePost, async (req, res) => {
   try {
-    const post = await db2.insert({...req.body, user_id: req.user });
+    const post = await db2.insert({...req.body, user_id: req.user.id });
     res.status(201).json(post);
   } catch(err) {
     res.status(500).json({ error: "Problem inserting post to the database" });
@@ -32,18 +32,13 @@ router.get('/', async (req, res) => {
   
 });
 
-router.get('/:id', validateUserId, async (req, res) => {
-  try {
-    const user = await db.getById(req.user);
-    res.status(200).json(user);
-  } catch(err) {
-    res.status(500).json({ error: "Problem getting user from the database" });
-  }
+router.get('/:id', validateUserId, (req, res) => {
+  res.status(200).json(req.user);
 });
 
 router.get('/:id/posts', validateUserId, async (req, res) => {
   try {
-    const posts = await db.getUserPosts(req.user);
+    const posts = await db.getUserPosts(req.user.id);
     res.status(200).json(posts);
   } catch(err) {
     res.status(500).json({ error: "Problem getting posts from the database" });
@@ -52,7 +47,7 @@ router.get('/:id/posts', validateUserId, async (req, res) => {
 
 router.delete('/:id', validateUserId, async (req, res) => {
   try {
-    const deleted = await db.remove(req.user);
+    const deleted = await db.remove(req.user.id);
     res.status(200).json(deleted);
   } catch(err) {
     res.status(500).json({ error: "Problem removing user from the database" });
@@ -61,7 +56,7 @@ router.delete('/:id', validateUserId, async (req, res) => {
 
 router.put('/:id', validateUser, validateUserId, async (req, res) => {
   try {
-    const user = await db.update(req.user, req.body);
+    const user = await db.update(req.user.id, req.body);
     res.status(200).json(user);
   } catch(err) {
     res.status(500).json({ error: "Problem updating user from the database" });
@@ -71,12 +66,17 @@ router.put('/:id', validateUser, validateUserId, async (req, res) => {
 //custom middleware
 
 async function validateUserId(req, res, next) {
-  const user = await db.getById(req.params.id);
-  if(!user) {
-    return res.status(404).json({ message: "invalid user id" });
+  try {
+    const user = await db.getById(req.params.id);
+    if (!user) {
+      return res.status(404).json({ message: "invalid user id" });
+    }
+    req.user = user;
+    next();
+  } catch(err) {
+    res.status(500).json({message: "error retrieving user from database"});
   }
-  req.user = req.params.id;
-  next();
+ 
 }
 
 function validateUser(req, res, next) {
